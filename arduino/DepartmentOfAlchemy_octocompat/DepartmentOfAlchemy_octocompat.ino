c496422e2d74ecdbf90724ad3eb37213cce4425f
Jesse Peterson <jesse@jpeterson.com>
2021-03-13 11:58:17 -0600

    Support uploading config and storing to SD card.

--- arduino/DepartmentOfAlchemy_octocompat/DepartmentOfAlchemy_octocompat.ino
+++ arduino/DepartmentOfAlchemy_octocompat/DepartmentOfAlchemy_octocompat.ino
@@ -17,6 +17,9 @@
  * quickly swapped if needed.
  */
 
+#include <SPI.h>
+#include <SD.h>
+
 #define DEBUG
 
 #ifdef DEBUG
@@ -28,6 +31,17 @@
   #define DEBUG_PRINTDEC(x)
   #define DEBUG_PRINTLN(x)
 #endif
+
+#define SD_SS 2
+
+File obcSequence;
+File obcConfig;
+bool obcConfigValid = true;
+
+#define OBC_CFG_FILENAME "OBC.CFG"
+#define OBC_SEQ_FILENAME "OBC.SEQ"
+
+#define CFG_LEN 9
 
 volatile uint8_t octo_stamp_buf[5] = {8,2,0,2,6};
 
@@ -90,12 +104,83 @@ void octo_check_serial() {
             }
             break;
 
+          case 'H':
+            // go hot
+            // TODO: implement
+            DEBUG_PRINTLN(F("Not implemented: H - go hot"));
+            Serial.println(F("Not implemented: H - go hot"));
+            flushReceive();
+            break;
+
+          case 'C':
+            // go cold
+            // TODO: implement
+            DEBUG_PRINTLN(F("Not implemented: C - go cold"));
+            Serial.println(F("Not implemented: C - go cold"));
+            flushReceive();
+            break;
+
+          case 'D':
+            // download eeprom
+            DEBUG_PRINTLN(F("Not implemented: D - download eeprom"));
+            Serial.println(F("Not implemented: D - download eeprom"));
+            flushReceive();
+            break;
+
+          case 'F':
+            // download config eeprom
+            DEBUG_PRINTLN(F("Not implemented: F - download config eeprom"));
+            Serial.println(F("Not implemented: F - download config eeprom"));
+            flushReceive();
+            break;
+
+          case 'S':
+            // receive sequence data
+            DEBUG_PRINTLN(F("Not implemented: S - upload sequence data"));
+            Serial.println(F("Not implemented: S - upload sequence data"));
+            flushReceive();
+            break;
+
+          case 'U':
+            // upload config
+            DEBUG_PRINTLN(F("Upload config"));
+            octo_receiveConfig();
+            break;
+
+          case 'P':
+            // ping back config
+            DEBUG_PRINTLN(F("Not implemented: P - ping back config"));
+            Serial.println(F("Not implemented: P - ping back config"));
+            flushReceive();
+            break;
+
+          case 'T':
+            // trigger
+            DEBUG_PRINTLN(F("Not implemented: T - trigger"));
+            Serial.println(F("Not implemented: T - trigger"));
+            flushReceive();
+            break;
+
+          case 'M':
+            // manual output
+            DEBUG_PRINTLN(F("Not implemented: M - manual output"));
+            Serial.println(F("Not implemented: M - manual output"));
+            flushReceive();
+            break;
+
+          case 'O':
+            // stamp OK
+            DEBUG_PRINTLN(F("Not implemented: O - stamp OK"));
+            Serial.println(F("Not implemented: O - stamp OK"));
+            flushReceive();
+            break;
+
           default:
             DEBUG_PRINT(F("unknown char: "));
             DEBUG_PRINTLN(b);
             Serial.print(F("unk char:"));
             Serial.println(b);
-            while (Serial.read() >= 0); // clear any input
+            flushReceive();
             break;
         }
       }
@@ -116,7 +201,16 @@ void setup() {
 
   DEBUG_PRINTLN(F("Department of Alchemy - octocompat"));
 
+  // let computer know we are listening
   Serial.println(F(".OBC"));
+
+  DEBUG_PRINTLN(F("Initializing SD card..."));
+
+  if (!SD.begin(SD_SS)) {
+    DEBUG_PRINTLN(F("initialization failed!"));
+    while (1);
+  }
+  DEBUG_PRINTLN(F("initialization done."));
 
   octo_report_config();
 
@@ -129,4 +223,94 @@ void setup() {
 
 void loop() {
   octo_check_serial();
+}
+
+/**
+ *  Clear the receive buffer of any characters
+ */
+void flushReceive() {
+  while (Serial.read() >= 0); // clear any input
+}
+
+uint16_t readInt16() {
+  uint8_t b[2];
+
+  for (int i = 0; i < 2; i++) {
+    while (Serial.available() == 0) {}
+    b[i] = Serial.read();
+  }
+  
+  return (uint16_t)(((uint16_t)b[1] << 8) | b[0]);
+}
+
+void octo_receiveConfig() {
+  uint8_t b;
+  uint16_t len;
+  while (Serial.available() == 0) {}
+
+  len = readInt16();
+
+  if (len != CFG_LEN) {
+    Serial.print(F("Unexpected config length: "));
+    Serial.println(len);
+    flushReceive();
+    return;
+  }
+
+  if (SD.exists(OBC_CFG_FILENAME)) {
+    SD.remove(OBC_CFG_FILENAME);
+  }
+  obcConfig = SD.open(OBC_CFG_FILENAME, FILE_WRITE);
+  if (!obcConfig) {
+    Serial.print(F("Unabled to open config file for writing: "));
+    Serial.println(OBC_CFG_FILENAME);
+    return;
+  }
+
+  for (uint16_t i = 0; i < len; i++) {
+
+    uint16_t count = 0;
+    while (Serial.available() == 0) {
+      count++;
+      if (count > 0xFFFE) {
+        Serial.print(F("Timeout waiting for config information: "));
+        Serial.println(count);
+        Serial.println("Please try again");
+        flushReceive();
+        return;
+      }
+    }
+    b = Serial.read();
+    obcConfig.write(b);
+  }
+
+  Serial.print(F("Received "));
+  Serial.print(len);
+  Serial.println(F(" config bytes"));
+  Serial.println(F("Please reconnect"));
+
+  // write 8 reserved bytes
+  for (uint8_t i = 0; i < 8; i++) {
+    obcConfig.write((uint8_t)0x00);
+  }
+
+  // write stamp
+  for (uint8_t i = 0; i < 5; i++) {
+    obcConfig.write(octo_stamp_buf[i]);
+  }
+
+  obcConfig.close();
+
+  if (!obcConfigValid) {
+    // clear any sequence file if the config data is not valid
+    if (SD.exists(OBC_SEQ_FILENAME)) {
+      SD.remove(OBC_SEQ_FILENAME);
+    }
+  }
+
+  obcConfigValid = true;
+
+  // OBF would now execute read_config to intialize variables based on the
+  // config data.
+  // TODO: initialize firmware based on translation of config values
 }
